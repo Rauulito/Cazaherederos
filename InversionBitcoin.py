592002ef3d1d2066c542127998743746b2fc5f64
Rauulito <7rnavarro7@gmail.com>
2022-05-30 18:12:26 +0200

    excepcion metida

--- InversionBitcoin.py
+++ InversionBitcoin.py
@@ -1,3 +1,5 @@
-def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
+def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros): #hacemos una funcion para ver cuando el valor de bitcoin cae por debajo de 30000
     euros_value = bitcoin_amount * bitcoin_value_euros
     return euros_value
+    if(euros_value < 30000):
+        print("El valor de euros es menor que 30000")
