118204eb2804847faeae652474ad1bf0a88eea0d
Rauulito <7rnavarro7@gmail.com>
2022-05-31 13:49:37 +0200

    cambiado

--- InversionBitcoin.py
+++ InversionBitcoin.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from tkinter import ttk
+
 #hacemos una funcion para ver cuando el valor de bitcoin cae por debajo de 30000
 def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
     euros_value = bitcoin_amount * bitcoin_value_euros
@@ -8,5 +11,11 @@ def bitcoinToEuros(bitcoin_amount, bitcoin_value_euros):
 #Probamos la funcion metiendo los datos por teclado
 bitcoin_amount = int(input("Introduce la cantidad de bitcoin: ")) #leemos la cantidad de bitcoin
 bitcoin_value_euros = float(input("Introduce el valor de bitcoin en euros: ")) #leemos el valor de bitcoin en euros
-euros_value = bitcoinToEuros(bitcoin_amount, bitcoin_value_euros) #llamamos a la funcion100
+euros_value = bitcoinToEuros(bitcoin_amount, bitcoin_value_euros) #llamamos a la funcion
 print("El valor de euros es: ", euros_value)
+
+#Probamos la funcion mediante interfaz grafica(tkinter)
+root = Tk()
+root.title("Inversion en Bitcoin")
+root.geometry("300x200")
+
