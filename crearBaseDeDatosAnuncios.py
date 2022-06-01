b85cbce22ae5ab9a3b2aebe567b760c84173f917
Rauulito <7rnavarro7@gmail.com>
2022-06-01 20:28:49 +0200

    comentarios actualizados

--- crearBaseDeDatosAnuncios.py
+++ crearBaseDeDatosAnuncios.py
@@ -1,5 +1,12 @@
 import sqlite3
 
+#Este programa solo sirve para generar la Base de Datos vacía donde después, y mediante otros programas
+# se guardaran los anuncios que se vayan publicando
+
+##############################################################################################################
+# IMPORTANTE: SOLO HAY QUE EJECUTARLO UNA VEZ, Y ANTES DE EMPEZAR A EJECUTAR OTROS PROGRAMAS QUE LA UTILICEN #
+##############################################################################################################
+
 # Objeto que representa la conexión a la base.
 conexion = sqlite3.connect('anuncios.db')
 
@@ -10,7 +17,11 @@ with conexion:
     cursor = conexion.cursor()
 
     # Ejecución de un script SQL para crear una tabla.
-
+    # Esta TABLA se llama Anuncios y contiene las siguientes columnas:
+    #    un Identificador entero que debe ser único entre todos, sera clave autoincrementada
+    #    el Nombre del NFT como campo de texto
+    #    el Nivel de Riesgo del NFT como campo de texto
+    #    el Vendedor que publica el anuncio como campo de texto
 
     orden="CREATE TABLE Anuncio(Id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Riesgo TEXT, Vendedor TEXT)"
     cursor.execute(orden)
