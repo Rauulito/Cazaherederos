a26741049e8cf8034151d0f9cf9489de5558e012
Rauulito <7rnavarro7@gmail.com>
2022-06-01 20:24:32 +0200

    actualizado

--- crearBaseDeDatosAnuncios.py
+++ crearBaseDeDatosAnuncios.py
@@ -3,3 +3,14 @@ import sqlite3
 # Objeto que representa la conexión a la base.
 conexion = sqlite3.connect('anuncios.db')
 
+# Uso de la palabra clave 'with' que cerrará automáticamente la conexión al final del bloque.
+with conexion:
+
+    # Recuperación del cursor de la conexión.
+    cursor = conexion.cursor()
+
+    # Ejecución de un script SQL para crear una tabla.
+
+
+    orden="CREATE TABLE Anuncio(Id INTEGER PRIMARY KEY AUTOINCREMENT, Nombre TEXT, Riesgo TEXT, Vendedor TEXT)"
+    cursor.execute(orden)
