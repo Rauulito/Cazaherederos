a37f0a1fac4fc7e0938e8dd9eb243cf3d8c41d9f
Rauulito <7rnavarro7@gmail.com>
2022-06-02 13:15:55 +0200

    metemos clase anuncio

--- anuncio.py
+++ anuncio.py
@@ -1,4 +1,10 @@
 
+#Clase cuyas instancias se almacenaran en una base de datos
+class Anuncio:
+    def __init__(self, nombre, riesgo, vendedor):
+        self.nombre = nombre
+        self.riesgo = riesgo
+        self.vendedor = vendedor
 
 
 
