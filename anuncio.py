f57ed35308ac03202fc1f93589f30e93b9746a44
Rauulito <7rnavarro7@gmail.com>
2022-06-02 13:46:58 +0200

    funcion nivelriesgo metida

--- anuncio.py
+++ anuncio.py
@@ -1,3 +1,4 @@
+import random
 
 #Clase cuyas instancias se almacenaran en una base de datos
 class Anuncio:
@@ -6,6 +7,16 @@ class Anuncio:
         self.riesgo = riesgo
         self.vendedor = vendedor
 
+#Hacemos una funcion predefinida para determinar una clasificación de criptomonadas por su nivel de riesgo
+#Como no tenemos ningun criterio definido vamos a hacerlo de forma aleatoria
+def Nivel_Riesgo(nft):
+    numero=random.randint(1,3)
+    if numero==1:
+        riesgo="Bajo"
+    elif numero==2:
+        riesgo="Medio"
+    else:
+        riesgo="Alto"
+    return riesgo
 
-
-
+print(Nivel_Riesgo("luis"))#Prueba para que funcione Nivel_Riesgo
